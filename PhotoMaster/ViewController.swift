abccd2ac9768579aed9ff25a0993c482af1b210d
Karin <karin.03k35@gmail.com>
2016-02-07 09:53:13 +0900

    first commit

--- PhotoMaster/ViewController.swift
+++ PhotoMaster/ViewController.swift
@@ -8,12 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UINavigationControllerDelegate,UIImagePickerControllerDelegate {
 
+    
+    @IBOutlet var photoimageview :UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
+    func precentPikerController(sourceType: UIImagePickerControllerSourceType){
+        if UIImagePickerController.isSourceTypeAvailable(sourceType){
+            let picker = UIImagePickerController()
+            
+            picker.sourceType = sourceType
+            
+            picker.delegate = self
+            
+            self.presentViewController(picker, animated: true, completion: nil)
+            
+        
+        }
+    }
+    func imagePickerController(picker: UIImagePickerController, didFinishPickerImage image:UIImage!,editingInfo:NSDictionary){
+        self.dismissViewControllerAnimated(true,completion:nil)
+        
+        photoimageview.image = image
+    
+    }
+    
+    @IBAction func selectButtonTapped(sender:UIButton){
+        let atertController = UIAlertController(title:"画像の表示",message: nil,preferredStyle: .ActionSheet)
+        
+    }
+    
+
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
